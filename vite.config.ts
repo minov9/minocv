865bba1ae8c87a4aad499b7417cc6d9a5c7038b4
minov9 <minov9@users.noreply.github.com>
2026-02-08 18:24:56 +0800

    fix: 修复 Vite base 路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/minocv/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 3000,
